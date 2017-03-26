30ef37777940519b0933a3bff0fd73bd600f4511
PazuzuR <insolencer@gmail.com>
2017-03-26 22:04:05 +0300

    Project v. 1

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# CleaningData
-Getting and Cleaning Data Course Project
+# Getting and Cleaning Data Course Project README
+*- read.table is used for reading data in steps 1.1-1.3*
+*- dplyr is used in this project*
+
+The goal of this project is to prepare tidy data that can be used for later analysis.
+
+The written script does following.
+1. Merges the training and the test sets to create one data set.
+2. Extracts only the measurements on the mean and standard deviation for each measurement.
+3. Uses descriptive activity names to name the activities in the data set
+4. Appropriately labels the data set with descriptive variable names.
+5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.The result of this step is text file "tidy_data.txt".
+
+##### Algorithm
+
+###### 1. Get data
+
+1. First of all we need to read common data - features (variables names in fact) and activity labels.
+2. The next step is to read test data (data itself, then activities, then subjects).
+3. In the same manner we need to read train data.
+4. Next, we should row bind test and train data - separately data, activities and subjects.
+5. Now we have all needed data and can make it tidy.
+
+###### 2. Make data tidy
+
+1. Set names for variables from features read in step 1.1.
+2. Merge activity labels read in step 1.1 with activities.
+3. Using grep filter out only mean and std columns.
+4. Make variables names tidy.
+5. Combine data and labels by columns.
+6. Rename subject and activity variables (columns) with descriptive names.
+7. Make summary (mean values) in tidy dataset grouped by subject and activity.
---
+++ run_analysis.R
@@ -0,0 +1,41 @@
+library(dplyr)
+
+# 1. read common data
+# 	1.1. read features (eq. variables names)
+features <- read.table("UCI HAR Dataset/features.txt")
+#	1.2. read activity labels
+activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt")
+
+# 2. read test data (data, then activity labels, then subjects)
+test <- read.table("UCI HAR Dataset/test/X_test.txt")
+test_labels <- read.table("UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
+
+# 3. read train data (data, then activity labels, then subjects)
+train <- read.table("UCI HAR Dataset/train/X_train.txt")
+train_labels <- read.table("UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
+
+# 4. row bind test and train data
+data <- rbind(test, train)
+data_labels <- rbind(test_labels, train_labels)
+subject_data <- rbind(subject_test, subject_train)
+
+# 5. tide data
+# 	5.1. name variables
+names(data) <- features$V2
+#	5.2. name activities
+data_labels <- merge(data_labels, activity_labels, by="V1", sort=FALSE)
+#	5.3. get only mean and std columns
+tidy_data <- data[grep("mean|std", names(data))]
+#	5.4. make variables names tidy
+names(tidy_data) <- gsub("[[:punct:]]","",gsub("-s","-S",gsub("-m","-M",names(tidy_data))))
+#	5.5. combine variables and labels
+tidy_data <- bind_cols(as.data.frame(as.factor(subject_data$V1)), as.data.frame(as.factor(data_labels$V2)), tidy_data)
+#	5.6. rename labels columns with descriptive names
+names(tidy_data)[1:2] <- c("subject", "activity")
+#	5.7. make summary
+tidy_data_summary <- tidy_data %>% group_by(subject, activity) %>% summarize_each(funs(mean))
+
+# 6. uncomment next line to write file
+#write.table(tidy_data_summary,"tidy_data.txt",row.name=FALSE)
